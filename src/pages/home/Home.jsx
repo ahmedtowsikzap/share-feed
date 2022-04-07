1f493069e46cac1bc42a8246b2469357f780c3ba
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-08 00:31:26 +0600

    setup fetching posts from server

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
-import "./Home.css"
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <>
-        <Header/>
-        <div className="home">
-             <Posts/>
-             <Sidebar/>
-        </div>
-
-        </>
-    );
+  const [posts, setposts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      setposts(res.data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
+    </>
+  );
 };
 
 export default Home;
