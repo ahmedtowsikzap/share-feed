f164c98a9cb2996c1ceef0e3a8eb56a988df0d7b
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-08 01:06:03 +0600

    mirnor change

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -13,16 +13,12 @@ const Post = ({post}) => {
                 <div className="postCats">{
 
                     post.categories.map((c)=>(
-
                     <span className="postCat">{c.name}</span>
-
                     ))}
                 </div>
                 <Link to={`/post/${post._id}`} className="link">
                 <span className="postTitle">{post.title}</span>
-
-                </Link>
-                
+                </Link> 
                 <hr />
                 <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
                 <p className="postDesc">{post.desc}</p>
