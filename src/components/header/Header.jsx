7e474e9ec6ce1f666fd578b8548f14c21e1845a2
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-11 17:57:51 +0600

    header section fixing

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,16 @@ import "./Header.css"
 const Header = () => {
     return (
         <div className="header">
-            <div className="headerTitles">
-            <span className="headerTitlesm"> <span className="big">Share</span> Feed</span>
-            <span className="headerTitleLg">stories</span>
-
-            </div>
-            <img className="headerImg" src="https://images.unsplash.com/photo-1532105111962-e23707867985?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
-        </div>
+      <div className="headerTitles">
+        <span className="headerTitleSm">Share & Feed</span>
+        <span className="headerTitleLg">Story</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.unsplash.com/photo-1532105111962-e23707867985?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        alt=""
+      />
+    </div>
     );
 };
 
