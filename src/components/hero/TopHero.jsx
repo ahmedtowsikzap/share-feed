855a8a1ca8696a2e6463cca1688a239805291a45
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-05-13 21:30:10 +0600

    thero

---
+++ src/components/hero/TopHero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopHero = () => {
+    return (
+        <div>
+            <h1>TOPhero or a banner section page</h1>
+        </div>
+    );
+};
+
+export default TopHero;
