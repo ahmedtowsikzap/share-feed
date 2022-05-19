f79e9173d98a697c05490eaa249804e6393e04d3
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-05-19 22:53:38 +0600

    cancel

--- src/components/hero/TopHero.jsx
+++ src/components/hero/TopHero.jsx
@@ -1,14 +1,10 @@
+import { Link } from "react-router-dom";
 import "./TopHero.css"
 
 const TopHero = () => {
     return (
         <div>
-          <section className="hero">
-              <h2>Share-Feed <br />Blog</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, eligendi veniam? Quos, blanditiis dolorem itaque ea quae voluptates numquam ducimus.</p>
-              
-
-          </section>
+         
         </div>
     );
 };
