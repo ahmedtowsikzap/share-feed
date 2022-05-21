e812709f27447158e0c1af8ef0ea682a01ce0fa1
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-05-21 22:11:29 +0600

    changed

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -28,8 +28,8 @@ export default function Sidebar() {
       <div className="sidebarItem">
         <span className="sidebarTitle">CATEGORIES</span>
         <ul className="sidebarList">
-          {cats.map((c) => (
-            <Link to={`/?cat=${c.name}`} className="link">
+          {cats.map((c,index) => (
+            <Link key={index} to={`/?cat=${c.name}`} className="link">
             <li className="sidebarListItem">{c.name}</li>
             </Link>
           ))}
